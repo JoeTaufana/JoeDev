9ab799b5c960edb7dd158a13ac65dd578360ffc5
JoeTaufana <taufanajoe@gmail.com>
2024-04-22 14:39:13 +0200

    maj js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
 
+
+// ************************* Accordion ************************/
 window.onload = () => {
   // On va chercher tous les éléments avec la classe "accordionElement"
   const elts = document.querySelectorAll(".accordionElement");
@@ -44,57 +46,17 @@ window.onload = () => {
   }
 }
 
-// **************Scroll des sections active ****
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navlinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
-        }
-    });
-
-    // ************** sticky navbar ****************************
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // ************* remove toggle icon and navbar when click navbar link (scroll)****************************
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
-
 // **************Scroll reveal ************************************************************
 ScrollReveal({
-    // reset: true,
+    reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200,
 });
 
-ScrollReveal().reveal('.home, .heading, .aboutImg, .skillsContent, .portfolioBox', { origin: 'top' });
-ScrollReveal().reveal('.container, .home a, .aboutItem p, .skillsExperience', { origin: 'bottom' });
-ScrollReveal().reveal('.home .socialMedia, .aboutItem, .skillsTechnology h2, .portfolioBox', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .aboutImg, .skillsContainer', { origin: 'right' });
+ScrollReveal().reveal('.home, .heading, .skillsContent, .portfolioBox', { origin: 'top' });
+ScrollReveal().reveal('.home a, aboutImg, .skillsExperience, .contactContent', { origin: 'bottom' });
+ScrollReveal().reveal('.socialMedia, .aboutItem, .skillsTechnology h2, .portfolioBox', { origin: 'left' });
+ScrollReveal().reveal('.skillsContainer, .contactForm', { origin: 'right' });
 
-// *************************writting typed js************************
-const typed = new Typed('.multiple-text', {
-    strings:    ["une Future Développeuse Frontend", 
-                "à la recherche d'un stage",
-                "autonome et très motivée",
-                "",
-                ],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,        
-});
+
